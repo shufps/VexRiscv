5ce7506fe4036848ba681e1bea8236b363d8aab4
ne23kaj2 <shufps80@gmail.com>
2019-11-20 07:36:02 +0100

    added variant of briey and MPU plugin

---
+++ src/main/scala/vexriscv/demo/ICCFPGA.scala
@@ -0,0 +1,379 @@
+package vexriscv.demo
+
+
+import vexriscv.plugin._
+import vexriscv._
+import vexriscv.ip.{DataCacheConfig, InstructionCacheConfig}
+import spinal.core._
+import spinal.lib._
+import spinal.lib.bus.amba3.apb._
+import spinal.lib.bus.amba4.axi._
+import spinal.lib.com.jtag.Jtag
+import spinal.lib.graphic.RgbConfig
+import spinal.lib.io.TriStateArray
+import spinal.lib.misc.HexTools
+import spinal.lib.soc.pinsec.{PinsecTimerCtrl, PinsecTimerCtrlExternal}
+import spinal.lib.system.debugger.{JtagAxi4SharedDebugger, JtagBridge, SystemDebugger, SystemDebuggerConfig}
+
+import scala.collection.mutable.ArrayBuffer
+
+
+case class ICCFPGAConfig(axiFrequency : HertzNumber,
+                       onChipRamSize : BigInt,
+                       cpuPlugins : ArrayBuffer[Plugin[VexRiscv]]
+                      )
+
+object ICCFPGAConfig{
+
+  def default = {
+    val config = ICCFPGAConfig(
+      axiFrequency = 50 MHz,
+      onChipRamSize  = 4 kB,
+      cpuPlugins = ArrayBuffer(
+        new PcManagerSimplePlugin(0x00000000l, false),
+        //          new IBusSimplePlugin(
+        //            interfaceKeepData = false,
+        //            catchAccessFault = true
+        //          ),
+        new IBusCachedPlugin(
+          resetVector = 0x00000000l,
+          prediction = STATIC,
+          compressedGen = true,
+          injectorStage = true,
+          config = InstructionCacheConfig(
+            cacheSize = 4096,
+            bytePerLine =32,
+            wayCount = 1,
+            addressWidth = 32,
+            cpuDataWidth = 32,
+            memDataWidth = 32,
+            catchIllegalAccess = true,
+            catchAccessFault = true,
+            asyncTagMemory = false,
+            twoCycleRam = true,
+            twoCycleCache = true
+          )
+          //            askMemoryTranslation = true,
+          //            memoryTranslatorPortConfig = MemoryTranslatorPortConfig(
+          //              portTlbSize = 4
+          //            )
+        ),
+        //                    new DBusSimplePlugin(
+        //                      catchAddressMisaligned = true,
+        //                      catchAccessFault = true
+        //                    ),
+        new DBusCachedPlugin(
+          config = new DataCacheConfig(
+            cacheSize         = 4096,
+            bytePerLine       = 32,
+            wayCount          = 1,
+            addressWidth      = 32,
+            cpuDataWidth      = 32,
+            memDataWidth      = 32,
+            catchAccessError  = true,
+            catchIllegal      = true,
+            catchUnaligned    = true
+          ),
+          memoryTranslatorPortConfig = null
+          //            memoryTranslatorPortConfig = MemoryTranslatorPortConfig(
+          //              portTlbSize = 6
+          //            )
+        ),
+/*        new StaticMemoryTranslatorPlugin(
+          ioRange      = _(31 downto 28) === 0xF
+        ),*/
+        new MemoryProtectionUnitPlugin(
+          ioRange         = _(31 downto 28) === 0xF,  // 0xF....... not-cached I/O 
+          romRange        = _(31 downto 28) === 0x0,  // 0x0....... ROM
+          ramRange        = _(31 downto 28) === 0x8,  // 0x8....... RAM
+          supervisorRange = _(27 downto 24) === 0x4   // 0xF4...... not-cached I/O supervisor, 0x.4...... cached supervisor
+        ),
+        new DecoderSimplePlugin(
+          catchIllegalInstruction = true
+        ),
+        new RegFilePlugin(
+          regFileReadyKind = plugin.SYNC,
+          zeroBoot = false
+        ),
+        new IntAluPlugin,
+        new SrcPlugin(
+          separatedAddSub = false,
+          executeInsertion = true
+        ),
+        new FullBarrelShifterPlugin,
+        new MulPlugin,
+        new DivPlugin,
+        new HazardSimplePlugin(
+          bypassExecute           = true,
+          bypassMemory            = true,
+          bypassWriteBack         = true,
+          bypassWriteBackBuffer   = true,
+          pessimisticUseSrc       = false,
+          pessimisticWriteRegFile = false,
+          pessimisticAddressMatch = false
+        ),
+        new BranchPlugin(
+          earlyBranch = false,
+          catchAddressMisaligned = true
+        ),
+        new CsrPlugin(
+          config = CsrPluginConfig(
+            catchIllegalAccess = false,
+            mvendorid      = null,
+            marchid        = null,
+            mimpid         = null,
+            mhartid        = null,
+            misaExtensionsInit = 66,
+            misaAccess     = CsrAccess.NONE,
+            mtvecAccess    = CsrAccess.NONE,
+            mtvecInit      = 0x00000020l,
+            mepcAccess     = CsrAccess.READ_WRITE,
+            mscratchGen    = false,
+            mcauseAccess   = CsrAccess.READ_ONLY,
+            mbadaddrAccess = CsrAccess.READ_ONLY,
+            mcycleAccess   = CsrAccess.NONE,
+            minstretAccess = CsrAccess.NONE,
+            ecallGen       = false,
+            wfiGenAsWait         = false,
+            ucycleAccess   = CsrAccess.NONE
+          )
+        ),
+        new YamlPlugin("cpu0.yaml")
+      )
+    )
+    config
+  }
+}
+
+
+
+class ICCFPGA(config: ICCFPGAConfig) extends Component{
+
+  //Legacy constructor
+  def this(axiFrequency: HertzNumber) {
+    this(ICCFPGAConfig.default.copy(axiFrequency = axiFrequency))
+  }
+
+  import config._
+  val debug = true
+  val interruptCount = 4
+    
+  val axiIOConfig = Axi4Config (
+    addressWidth = 32,
+    dataWidth = 32,
+    idWidth = 4
+  )
+
+  val io = new Bundle{
+    //Clocks / reset
+    val asyncReset = in Bool
+    val axiClk     = in Bool
+
+    //Main components IO
+    val jtag       = slave(Jtag())
+
+    val axiIO     = master(Axi4(axiIOConfig))
+
+    //Peripherals IO
+    val timerInterrupt = in Bool
+    val timerExternal = in(PinsecTimerCtrlExternal())
+    val coreInterrupt = in Bool
+  }
+
+  val resetCtrlClockDomain = ClockDomain(
+    clock = io.axiClk,
+    config = ClockDomainConfig(
+      resetKind = BOOT
+    )
+  )
+
+  val resetCtrl = new ClockingArea(resetCtrlClockDomain) {
+    val systemResetUnbuffered  = False
+//    val coreResetUnbuffered = False
+
+    //Implement an counter to keep the reset axiResetOrder high 64 cycles
+    // Also this counter will automaticly do a reset when the system boot.
+    val systemResetCounter = Reg(UInt(6 bits)) init(0)
+    when(systemResetCounter =/= U(systemResetCounter.range -> true)){
+      systemResetCounter := systemResetCounter + 1
+      systemResetUnbuffered := True
+    }
+    when(BufferCC(io.asyncReset)){
+      systemResetCounter := 0
+    }
+
+    //Create all reset used later in the design
+    val systemReset  = RegNext(systemResetUnbuffered)
+    val axiReset     = RegNext(systemResetUnbuffered)
+  }
+
+  val axiClockDomain = ClockDomain(
+    clock = io.axiClk,
+    reset = resetCtrl.axiReset,
+    frequency = FixedFrequency(axiFrequency) //The frequency information is used by the SDRAM controller
+  )
+
+  val debugClockDomain = ClockDomain(
+    clock = io.axiClk,
+    reset = resetCtrl.systemReset,
+    frequency = FixedFrequency(axiFrequency)
+  )
+
+
+  val axi = new ClockingArea(axiClockDomain) {
+    val ram = Axi4SharedOnChipRam(
+      dataWidth = 32,
+      byteCount = 128 kB,
+      idWidth = 4
+    )
+
+    val svram = Axi4SharedOnChipRam(
+      dataWidth = 32,
+      byteCount = 4 kB,
+      idWidth = 4
+    )
+
+    svram.ram.generateAsBlackBox()
+
+    val rom = Axi4SharedOnChipRam(
+      dataWidth = 32,
+      byteCount = 128 kB,
+      idWidth = 4
+    )
+
+    ram.ram.generateAsBlackBox()
+    rom.ram.generateAsBlackBox()
+
+    val apbBridge = Axi4SharedToApb3Bridge(
+      addressWidth = 20,
+      dataWidth    = 32,
+      idWidth      = 4
+    )
+
+    val timerCtrl = PinsecTimerCtrl()
+
+
+    val core = new Area{
+      val config = VexRiscvConfig(
+        plugins = cpuPlugins += new DebugPlugin(debugClockDomain)
+      )
+
+      val cpu = new VexRiscv(config)
+      var iBus : Axi4ReadOnly = null
+      var dBus : Axi4Shared = null
+      for(plugin <- config.plugins) plugin match{
+        case plugin : IBusSimplePlugin => iBus = plugin.iBus.toAxi4ReadOnly()
+        case plugin : IBusCachedPlugin => iBus = plugin.iBus.toAxi4ReadOnly()
+        case plugin : DBusSimplePlugin => dBus = plugin.dBus.toAxi4Shared()
+        case plugin : DBusCachedPlugin => dBus = plugin.dBus.toAxi4Shared(true)
+        case plugin : CsrPlugin        => {
+          plugin.externalInterrupt := BufferCC(io.coreInterrupt)
+          plugin.timerInterrupt := BufferCC(io.timerInterrupt) //timerCtrl.io.interrupt
+        }
+        case plugin : DebugPlugin      => debugClockDomain{
+          resetCtrl.axiReset setWhen(RegNext(plugin.io.resetOut))
+          io.jtag <> plugin.io.bus.fromJtag()
+        }
+        case _ =>
+      }
+    }
+
+    val axiCrossbar = Axi4CrossbarFactory()
+
+    axiCrossbar.addSlaves(
+      rom.io.axi           -> (0x00000000L,   128 kB),
+      ram.io.axi           -> (0x80000000L,   128 kB),
+      svram.io.axi         -> (0x84000000L,   4 kB),
+      apbBridge.io.axi     -> (0xF0000000L,   1 MB),
+      io.axiIO             -> (0xF1000000L,   64 MB)
+    )
+
+    axiCrossbar.addConnections(
+      core.iBus       -> List(rom.io.axi),
+      core.dBus       -> List(rom.io.axi, ram.io.axi, svram.io.axi, apbBridge.io.axi, io.axiIO)
+    )
+
+
+    axiCrossbar.addPipelining(apbBridge.io.axi)((crossbar,bridge) => {
+      crossbar.sharedCmd.halfPipe() >> bridge.sharedCmd
+      crossbar.writeData.halfPipe() >> bridge.writeData
+      crossbar.writeRsp             << bridge.writeRsp
+      crossbar.readRsp              << bridge.readRsp
+    })
+
+    axiCrossbar.addPipelining(rom.io.axi)((crossbar,ctrl) => {
+      crossbar.sharedCmd.halfPipe()  >>  ctrl.sharedCmd
+      crossbar.writeData            >/-> ctrl.writeData
+      crossbar.writeRsp              <<  ctrl.writeRsp
+      crossbar.readRsp               <<  ctrl.readRsp
+    })
+
+    axiCrossbar.addPipelining(ram.io.axi)((crossbar,ctrl) => {
+      crossbar.sharedCmd.halfPipe()  >>  ctrl.sharedCmd
+      crossbar.writeData            >/-> ctrl.writeData
+      crossbar.writeRsp              <<  ctrl.writeRsp
+      crossbar.readRsp               <<  ctrl.readRsp
+    })
+
+    axiCrossbar.addPipelining(svram.io.axi)((crossbar,ctrl) => {
+      crossbar.sharedCmd.halfPipe()  >>  ctrl.sharedCmd
+      crossbar.writeData            >/-> ctrl.writeData
+      crossbar.writeRsp              <<  ctrl.writeRsp
+      crossbar.readRsp               <<  ctrl.readRsp
+    })
+
+    axiCrossbar.addPipelining(core.dBus)((cpu,crossbar) => {
+      cpu.sharedCmd             >>  crossbar.sharedCmd
+      cpu.writeData             >>  crossbar.writeData
+      cpu.writeRsp              <<  crossbar.writeRsp
+      cpu.readRsp               <-< crossbar.readRsp //Data cache directly use read responses without buffering, so pipeline it for FMax
+    })
+
+    axiCrossbar.build()
+
+
+    val apbDecoder = Apb3Decoder(
+      master = apbBridge.io.apb,
+      slaves = List(
+        timerCtrl.io.apb -> (0x20000, 4 kB)
+      )
+    )
+  }
+
+  io.timerExternal  <> axi.timerCtrl.io.external
+}
+
+//DE1-SoC
+object ICCFPGA{
+  def main(args: Array[String]) {
+    val config = SpinalConfig()
+    config.generateVerilog({
+      val toplevel = new ICCFPGA(ICCFPGAConfig.default)
+      toplevel
+    })
+  }
+}
+
+//DE1-SoC with memory init
+object ICCFPGAWithMemoryInit{
+  def main(args: Array[String]) {
+    val config = SpinalConfig()
+    config.generateVerilog({
+      val toplevel = new ICCFPGA(ICCFPGAConfig.default)
+      HexTools.initRam(toplevel.axi.ram.ram, "src/main/ressource/hex/muraxDemo.hex", 0x80000000l)
+      toplevel
+    })
+  }
+}
+
+
+//DE0-Nano
+object ICCFPGADe0Nano{
+  def main(args: Array[String]) {
+    val config = SpinalConfig()
+    config.generateVerilog({
+      val toplevel = new ICCFPGA(ICCFPGAConfig.default.copy())
+      toplevel
+    })
+  }
+}
---
+++ src/main/scala/vexriscv/plugin/MemoryProtectionUnitPlugin.scala
@@ -0,0 +1,51 @@
+package vexriscv.plugin
+
+import vexriscv.{VexRiscv, _}
+import spinal.core._
+import spinal.lib._
+
+import scala.collection.mutable.ArrayBuffer
+case class MemoryProtectionUnitPort(bus : MemoryTranslatorBus, priority : Int)
+
+class MemoryProtectionUnitPlugin(ioRange : UInt => Bool, romRange : UInt => Bool, ramRange : UInt => Bool, supervisorRange : UInt => Bool) extends Plugin[VexRiscv] with MemoryTranslator {
+  val portsInfo = ArrayBuffer[MemoryProtectionUnitPort]()
+
+  override def newTranslationPort(priority : Int,args : Any): MemoryTranslatorBus = {
+//    val exceptionBus = pipeline.service(classOf[ExceptionService]).newExceptionPort(stage)
+    val port = MemoryProtectionUnitPort(MemoryTranslatorBus(),priority)
+    portsInfo += port
+    port.bus
+  }
+
+  override def setup(pipeline: VexRiscv): Unit = {
+  }
+
+  override def build(pipeline: VexRiscv): Unit = {
+    import pipeline._
+    import pipeline.config._
+    import Riscv._
+
+    val core = pipeline plug new Area {
+      val ports = for ((port, portId) <- portsInfo.zipWithIndex) yield new Area {
+        val privilegeService = pipeline.serviceElse(classOf[PrivilegeService], PrivilegeServiceDefault())
+
+        port.bus.rsp.physicalAddress := port.bus.cmd.virtualAddress
+
+        when(supervisorRange(port.bus.rsp.physicalAddress)) {
+          port.bus.rsp.allowRead := !privilegeService.isUser()
+          port.bus.rsp.allowWrite := !privilegeService.isUser()
+          port.bus.rsp.allowExecute := !privilegeService.isUser()
+        } otherwise {
+          port.bus.rsp.allowRead := True
+          // last term for debugging upload
+          port.bus.rsp.allowWrite := !romRange(port.bus.rsp.physicalAddress) || !privilegeService.isUser()
+          port.bus.rsp.allowExecute := romRange(port.bus.rsp.physicalAddress) 
+        }
+        port.bus.rsp.isIoAccess := ioRange(port.bus.rsp.physicalAddress)
+        port.bus.rsp.exception := False
+        port.bus.rsp.refilling := False
+        port.bus.busy := False
+      }
+    }
+  }
+}
